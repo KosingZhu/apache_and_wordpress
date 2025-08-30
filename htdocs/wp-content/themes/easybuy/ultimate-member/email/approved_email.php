35d76020214dc1908ab6a0888f2ebb5304072b50
kosing <447898455@qq.com>
2025-08-31 00:59:49 +0800

    增加会员管理插件和全局日志接口，日志管理插件

---
+++ htdocs/wp-content/themes/easybuy/ultimate-member/email/approved_email.php
@@ -0,0 +1,20 @@
+<div style="max-width: 560px;padding: 20px;background: #ffffff;border-radius: 5px;margin: 40px auto;font-family: Open Sans,Helvetica,Arial;font-size: 15px;color: #666">
+<div style="color: #444444;font-weight: normal">
+<div style="text-align: center;font-weight: 600;font-size: 26px;padding: 10px 0;border-bottom: solid 3px #eeeeee">{site_name}<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> {:域名}</span></span></div>
+<div style="clear: both"> </div>
+</div>
+<div style="padding: 0 30px 30px 30px;border-bottom: 3px solid #eeeeee">
+<div style="padding: 30px 0;font-size: 24px;text-align: center;line-height: 40px">Thank you for signing up!<span class="yd-translate-container"><span class="yd-wrapper-block yd-highlight">感谢您的报名！</span></span><span>Your account is now approved.<span class="yd-translate-container"><span class="yd-wrapper-block yd-highlight">您的帐户已被批准。</span></span></span></div>
+<div style="padding: 10px 0 50px 0;text-align: center"><a style="background: #555555;color: #fff;padding: 12px 30px;text-decoration: none;border-radius: 3px;letter-spacing: 0.3px" href="{login_url}">Login to our site<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> 登入我们的网站</span></span></a></div>
+<div style="padding: 0 0 15px 0">
+<div style="background: #eee;color: #444;padding: 12px 15px;border-radius: 3px;font-weight: bold;font-size: 16px">Account Information<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> 账户信息</span></span></div>
+<div style="padding: 10px 15px 0 15px;color: #333"><span style="color: #999">Your account email:<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> 您的账户邮箱：</span></span></span> <span style="font-weight: bold">{email}<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> {邮件}</span></span></span></div>
+<div style="padding: 10px 15px 0 15px;color: #333"><span style="color: #999">Your account username:<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> 您的帐户名称：</span></span></span> <span style="font-weight: bold">{username}</span></div>
+<div style="padding: 10px 15px 0 15px;color: #333"><span style="color: #999">Set your password:</span> <span style="font-weight: bold"><a style="color: #3ba1da;text-decoration: none" href="{password_reset_link}">{password_reset_link}</a></span><span class="yd-translate-container"><span class="yd-wrapper-block yd-highlight">设置密码：{password_reset_link}</span></span></div>
+</div>
+</div>
+<div style="color: #999;padding: 20px 30px">
+<div>Thank you!<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> 谢谢你！</span></span></div>
+<div>The <a style="color: #3ba1da;text-decoration: none" href="{site_url}">{site_name}</a> Team<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> {site_name}团队</span></span></div>
+</div>
+</div>
---
+++ htdocs/wp-content/themes/easybuy/ultimate-member/email/rejected_email.php
@@ -0,0 +1,14 @@
+<div style="max-width: 560px;padding: 20px;background: #ffffff;border-radius: 5px;margin: 40px auto;font-family: Open Sans,Helvetica,Arial;font-size: 15px;color: #666">
+<div style="color: #444444;font-weight: normal">
+<div style="text-align: center;font-weight: 600;font-size: 26px;padding: 10px 0;border-bottom: solid 3px #eeeeee">{site_name}<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> {:域名}</span></span></div>
+<div style="clear: both"> </div>
+</div>
+<div style="padding: 0 30px 30px 30px;border-bottom: 3px solid #eeeeee">
+<div style="padding: 30px 0;font-size: 24px;text-align: center;line-height: 40px">Your registration request has been rejected.<span class="yd-translate-container"><span class="yd-wrapper-block yd-highlight">您的注册请求已被拒绝。</span></span></div>
+<div style="padding: 15px;background: #eee;border-radius: 3px;text-align: center">Please feel free to apply at a future date or <a style="color: #3ba1da;text-decoration: none" href="mailto:{admin_email}">contact us</a> If you want your information to be reviewed again.<span class="yd-translate-container"><span class="yd-wrapper-block yd-highlight">请随时申请在未来的日期或联系我们，如果您希望您的信息被再次审查。</span></span></div>
+</div>
+<div style="color: #999;padding: 20px 30px">
+<div>Thank you!<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> 谢谢你！</span></span></div>
+<div>The <a style="color: #3ba1da;text-decoration: none" href="{site_url}">{site_name}</a> Team<span class="yd-translate-container"> <span class="yd-wrapper-inline yd-highlight"> {site_name}团队</span></span></div>
+</div>
+</div>
